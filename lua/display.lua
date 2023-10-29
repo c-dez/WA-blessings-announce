747e36495fc81456098a5ee9a0e08c999d21bce3
c-dez <hdezcruzcarlos@gmail.com>
2023-10-28 18:29:51 -0600

    first push

---
+++ lua/display.lua
@@ -0,0 +1,14 @@
+--display text custom function for trigger
+function()
+    local name
+    local confirmationText
+
+    local name, rank, icon, count, dispelType,
+     duration, expires, caster, isStealable,
+      nameplateShowPersonal, spellID, canApplyAura,
+       isBossDebuff, _, nameplateShowAll, timeMod, 
+       value1, value2, value3
+       =UnitBuff('unit', 'name' , 'INDEX', 'HELPFUL')
+
+
+end
