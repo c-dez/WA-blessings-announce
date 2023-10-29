cdcbb86201f37ac49c6b63466ca1d637a1842146
c-dez <hdezcruzcarlos@gmail.com>
2023-10-28 21:35:48 -0600

    regresa unitId en party 2 es correcto en party1 esta hardcode

--- lua/display.lua
+++ lua/display.lua
@@ -1,30 +1,35 @@
 --display text custom function for trigger
 function()
-      
-   local name 
+    local target
+    local name 
+    
     for i = 0, 5, 1 do
         if i == 1 then
-           for i = 1, 10, 1 do
-            name = UnitBuff('party1', i)
-            if name == 'Blessing of Freedom' then
-                return name
+            for i = 1, 10, 1 do
+                name = UnitBuff('party1', i)
+                if name == 'Blessing of Freedom' then
+                    target = 'party1'
+                    return target
+                end
             end
-           end
         end
-
+        
         if i ==2 then
             for i = 1, 10, 1 do
-                name = UnitBuff('party2', i)
+                local unitId = 'party2'
+                name = UnitBuff(unitId, i)
                 if name == 'Blessing of Freedom' then
-                    return name
+                    target = unitId
+                   return target
                 end
-               end
+            end
         end
     end
-    -- freedom muestra mensaje si se usa en party1 o party2
-    return name
+    
+    return target
     
 end
+-- returns target == que es el unitId 'party1'...'party2'
 
 
 
