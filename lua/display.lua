182879ff73ea2e5464ff148f32e62cb912c236dd
c-dez <hdezcruzcarlos@gmail.com>
2023-10-28 21:12:12 -0600

    freedom muestra mensaje si se usa en party1 o party2

--- lua/display.lua
+++ lua/display.lua
@@ -1,27 +1,62 @@
 --display text custom function for trigger
 function()
-    local name
-    local confirmationText
+      
+   local name 
+    for i = 0, 5, 1 do
+        if i == 1 then
+           for i = 1, 10, 1 do
+            name = UnitBuff('party1', i)
+            if name == 'Blessing of Freedom' then
+                return name
+            end
+           end
+        end
 
-    for i=0,3,1 -- check buff on  party 1...3
-    do
-        if i==1 then
-            local unitId = 'player' --changed for testing
+        if i ==2 then
             for i = 1, 10, 1 do
-                name = UnitBuff(unitId, i)
+                name = UnitBuff('party2', i)
                 if name == 'Blessing of Freedom' then
-                    print(name.. i)
                     return name
                 end
-            end
-            
+               end
         end
-       
-
-
     end
+    -- freedom muestra mensaje si se usa en party1 o party2
+    return name
+    
+end
 
 
 
+return name
 
 end
+
+
+-- olad code
+
+
+function()
+    local name
+    local confirmationText
+    for i=0,10,1
+    do
+        name = UnitBuff('target',i)  
+        
+        if name == 'Devotion Aura' then
+            confirmationText = name
+            print(name.. i)
+            return freedom 
+            
+        end
+        
+    end
+    
+    return confirmationText
+    
+    
+end
+
+
+
+
